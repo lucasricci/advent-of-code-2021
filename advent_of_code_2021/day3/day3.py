7fad0e873cfb34247f1bfe0b3fecf1c248b64f06
Lucas Ricci <lucasl.ricci@uol.com.br>
2021-12-06 22:14:20 -0300

    Commit Day 3

--- advent_of_code_2021/day3/day3.py
+++ advent_of_code_2021/day3/day3.py
@@ -1,24 +1,35 @@
 from collections import Counter
+from random import choice
 
 with open("input.txt") as f:
     binary = f.read().splitlines()
 
 
-def part_one():
-    gamma = []; epsilon = []
+def part_one() -> int:
+    gamma, epsilon = [], []
     chunks = [[row[col] for row in binary] for col in range(12)]
     for i in range(12):
         most = Counter(chunks[i]).most_common()[0][0]
         least = Counter(chunks[i]).most_common()[-1][0]
         gamma.append(most); epsilon.append(least)
-    print(int("".join(gamma), 2) * int("".join(epsilon), 2))
+    return int("".join(gamma), 2) * int("".join(epsilon), 2)
 
 
 def part_two():
-    bina = [[row[col] for row in binary] for col in range(1)]
-    print(bina)
+    oxygen_rate, carbon_rate, common = [], [], []
+    oxygen = [[row[col] for row in binary] for col in range(12)]
+    carbon = [[row[col] for row in binary] for col in range(12)]
+
+    for i in range(12):
+        most = Counter(oxygen[i]).most_common()[0][0]
+        common.append(most)
+
+    for n in range(12):
+        if binary[0][n] != common[n]:
+            binary.remove(binary[n])
+    print(binary)
 
 
 if __name__ == "__main__":
-    #part_one
+    print(part_one())
     part_two()
